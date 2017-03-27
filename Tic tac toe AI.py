89a70e4435742c402045da01071744557bf59f0d
Higiey <Hickinbothamtom@gmail.com>
2017-03-27 09:19:00 +0000

    Added some comments on the code

--- Tic tac toe AI.py
+++ Tic tac toe AI.py
@@ -33,6 +33,7 @@ place7 = ''
 place8 = ''
 place9 = ''
 
+#This is the game board
 def gameGUI():
 
 	print('-------------------------------------------------\n:\t\t:\t\t:\t\t:\n:\t',place1,'\t:\t',place2,'\t:\t',place3,'\t:\n:\t\t:\t\t:\t\t:')
@@ -40,6 +41,10 @@ def gameGUI():
 	print('-------------------------------------------------\n:\t\t:\t\t:\t\t:\n:\t',place7,'\t:\t',place8,'\t:\t',place9,'\t:\n:\t\t:\t\t:\t\t:')
 	print('-------------------------------------------------')
 
+
+#Check if a player has won.
+#This needs to be redone as it doesn't work at all.
+	#It says a player has won even when the line hasn't been completed with the same character
 def Winner(input, player1,player2):
 	if place1 and place2 and place3 in ['X'] or place4 and place5 and place6 in ['X'] or place7 and place8 and place9 in ['X'] or place1 and place4 and place7 in ['X'] or place2 and place5 and place8 in ['X'] or place3 and place6 and place9 in ['X'] or place1 and place5 and place9 in ['X'] or place3 and place5 and place7 in ['X']:
 		return 1
@@ -48,6 +53,8 @@ def Winner(input, player1,player2):
 	else:
 		return 0
 
+#This was an attempt at starting the AI, It does not work as intended
+	#Needs to be reworked
 def First_move(place1,place2,place3,place4,place5,place6,place7,place8,place9):
 	RandNum = random.randint(0,7)
 	print('Random Numer = ',RandNum)
@@ -70,162 +77,10 @@ def First_move(place1,place2,place3,place4,place5,place6,place7,place8,place9):
 			return 9
 		else:
 			return 0
-	elif place2 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 3
-		elif RandNum == 2:
-			return 4
-		elif RandNum == 3:
-			return 5
-		elif RandNum == 4:
-			return 6
-		elif RandNum == 5:
-			return 7
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place3 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 4
-		elif RandNum == 3:
-			return 5
-		elif RandNum == 4:
-			return 6
-		elif RandNum == 5:
-			return 7
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place4 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 3
-		elif RandNum == 3:
-			return 5
-		elif RandNum == 4:
-			return 6
-		elif RandNum == 5:
-			return 7
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place5 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 3
-		elif RandNum == 3:
-			return 4
-		elif RandNum == 4:
-			return 6
-		elif RandNum == 5:
-			return 7
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place6 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 3
-		elif RandNum == 3:
-			return 4
-		elif RandNum == 4:
-			return 6
-		elif RandNum == 5:
-			return 7
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place7 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 3
-		elif RandNum == 3:
-			return 4
-		elif RandNum == 4:
-			return 5
-		elif RandNum == 5:
-			return 6
-		elif RandNum == 6:
-			return 8
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place8 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 3
-		elif RandNum == 3:
-			return 4
-		elif RandNum == 4:
-			return 5
-		elif RandNum == 5:
-			return 6
-		elif RandNum == 6:
-			return 7
-		elif RandNum == 7:
-			return 9
-		else:
-			return 0
-	elif place9 in ['X']:
-		if RandNum == 0:
-			return 1
-		elif RandNum == 1:
-			return 2
-		elif RandNum == 2:
-			return 5
-		elif RandNum == 3:
-			return 4
-		elif RandNum == 4:
-			return 5
-		elif RandNum == 5:
-			return 6
-		elif RandNum == 6:
-			return 7
-		elif RandNum == 7:
-			return 8
-		else:
-			return 0
 gameGUI()
 while True:
 
-
+#This works perfectly fine, it inputs a users go and inputs it into the gui
 	if Gamemode in ['2','two']:
 		turn = input("Where do you want to go player 1?")
 		if turn in ['top left']:
@@ -249,14 +104,14 @@ while True:
 		else:
 			print("Wrong")
 		gameGUI()
-
+		#This doesn't work as the Winner() function doesn't work
 		if Winner(0,'Player 1 WINS!!!!','PLayer 2 WINS!!!!')>0:
 			if Winner(0,'Player 1 WINS!!!!','PLayer 2 WINS!!!!')==1:
 				print('Player 1 Wins!!!!')
 			elif Winner(0,'Player 1 WINS!!!!','PLayer 2 WINS!!!!')==2:
 				print('Player 2 Wins!!!!')
 			break
-
+#This is the same as the X component, it works. but there may be a way to improve it.
 		turn = input("Where do you want to go player 2?")
 		if turn in ['top left']:
 			place1 = 'O'
@@ -285,6 +140,9 @@ while True:
 			elif Winner(0,'Player 1 WINS!!!!','PLayer 2 WINS!!!!')==2:
 				print('Player 2 Wins!!!!')
 			break
+
+
+#This doesn't work, but I will keep it as a template and reminder.
 	elif Gamemode in ['1','one']:
 
 
